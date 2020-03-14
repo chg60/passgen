71f9b1086f3d254058a127e053cb744b413f13e0
Christian Gauthier <chg60@pitt.edu>
2020-03-14 17:05:21 -0400

    enhanced flexibility to particular use-cases

--- passgen.py
+++ passgen.py
@@ -10,33 +10,67 @@ import sys
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument("--length", type=int, default=12,
                     help="number of characters in password")
-parser.add_argument("--no-lower", action="store_true",
-                    help="don't use lowercase letters")
-parser.add_argument("--no-upper", action="store_true",
-                    help="don't use uppercase letters")
-parser.add_argument("--no-number", action="store_true",
-                    help="don't use numeric characters")
-parser.add_argument("--no-special", action="store_true",
-                    help="don't use special characters")
+parser.add_argument("--min-lower", type=int, default=1,
+                    help="minimum number of lowercase letters")
+parser.add_argument("--min-upper", type=int, default=1,
+                    help="minimum number of uppercase letters")
+parser.add_argument("--min-number", type=int, default=1,
+                    help="minimum number of numeric characters")
+parser.add_argument("--min-special", type=int, default=1,
+                    help="minimum number of special characters")
+parser.add_argument("--special", type=str,
+                    help="\"\"-enclosed chars to use instead of punctuation")
+
 
 def main(arguments):
     args = parser.parse_args(arguments)
-    password = ""
 
-    allowed_chars = []
-    if not args.no_lower:
-        allowed_chars.extend(string.ascii_lowercase)
-    if not args.no_upper:
-        allowed_chars.extend(string.ascii_uppercase)
-    if not args.no_number:
-        allowed_chars.extend(string.digits)
-    if not args.no_special:
-        allowed_chars.extend(string.punctuation)
+    # Sanity check that sum of minimum char counts is not larger than
+    # specified password length.
+    if sum([args.min_lower, args.min_upper,
+            args.min_number, args.min_special]) > args.length:
+        raise ValueError(f"Cannot create password of length {args.length} "
+                         f"and satisfy other given criteria.")
+
+    # Store the allowed alphabets in a dictionary with integer keys
+    allowed_chars = dict()
+    pw = ""
+
+    if args.min_lower > 0:
+        allowed_chars[len(allowed_chars)] = string.ascii_lowercase
+        pw += "".join(random.choices(string.ascii_lowercase, k=args.min_lower))
+    if args.min_upper > 0:
+        allowed_chars[len(allowed_chars)] = string.ascii_uppercase
+        pw += "".join(random.choices(string.ascii_uppercase, k=args.min_upper))
+    if args.min_number > 0:
+        allowed_chars[len(allowed_chars)] = string.digits
+        pw += "".join(random.choices(string.digits, k=args.min_number))
+    if args.min_special > 0:
+        if not args.special:
+            allowed_chars[len(allowed_chars)] = string.punctuation
+            pw += "".join(random.choices(string.punctuation, k=args.min_special))
+        else:
+            allowed_chars[len(allowed_chars)] = args.special
+            pw += "".join(random.choices(args.special, k=args.min_special))
 
     if not allowed_chars:
-        raise ValueError("Cannot generate password with 0 allowed characters.")
+        raise ValueError(f"Cannot create password of length {args.length} "
+                         f"with no allowed characters.")
 
-    print("".join(random.choices(allowed_chars, k=args.length)))
+    # Get the password up to the final length with randomly chosen chars
+    # from randomly chosen allowed alphabets
+    while len(pw) < args.length:
+        key = random.randrange(0, len(allowed_chars))
+        pw += random.choice(allowed_chars[key])
+
+    # Shuffle the password a few (0-10) times by using random sampling on
+    # the password without replacement
+    times = random.randint(0, 10)
+    while times > 0:
+        pw = "".join(random.sample(pw, len(pw)))
+        times -= 1
+
+    print(pw)
 
 
 if __name__ == "__main__":
